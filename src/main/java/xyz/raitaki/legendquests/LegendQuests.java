877c14ed84357fcb39d103635fd45cc62a0f934c
RaiTaki <raitakihg@gmail.com>
2024-01-08 16:10:49 +0300

    Initial commit

---
+++ src/main/java/xyz/raitaki/legendquests/LegendQuests.java
@@ -0,0 +1,17 @@
+package xyz.raitaki.legendquests;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LegendQuests extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logi
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
